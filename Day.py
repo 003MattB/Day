9be11aa96a57198cae564aed50aad30f4ab0032d
Matt Bundy <mattb@protech10.(none)>
2017-09-11 23:51:33 -0500

    Fixed bugs in __add__ method

--- Day.py
+++ Day.py
@@ -2,6 +2,7 @@ class Day(object):
 
     def __init__(self,date):
         self.__date = date
+        #ranges represents the days in each month
         self.__ranges = {'01' : 31,
                          '02' : 28,
                          '03' : 31,
@@ -14,55 +15,52 @@ class Day(object):
                          '10' : 31,
                          '11' : 30,
                          '12' : 31}
-        self.updateRanges()
+        #if its a leap year '02' (Feb) will be changed to 29
+        self.__updateRanges()
 
-    def __add__(self,num):
-        ## TODO finish this
+    def addOne(self):
+        #increments self by 1 day
         year = int(self.__date[0:4])
         month = int(self.__date[5:7])
         day = int(self.__date[8:10])
-        y2add = 0
-        m2add = 0
-        d2add = 0
-        if self.isLeapYear():
-            y2add = num // 366
-            num %= 366
-        else:
-            y2add = num // 365
-            num %= 365
-        
-        m2add = num // self.__ranges[self.__date[5:7]] 
-        num %= self.__ranges[self.__date[5:7]]
-        d2add = num
-
-        year += y2add
-        month += m2add
-        day += d2add
-        if day > self.__ranges[self.__date[5:7]]:
+        day += 1
+        if day > self.__ranges["{0:0>2}".format(month)]:
             month += 1
-            day = day - self.__ranges[self.__date[5:7]]
-        if month > 12:
-            year += 1
-            month = 1
+            if month > 12:
+                year += 1
+                month = 1
+            day = 1
+        #stringify and add a leading zero if month or day < 10
         if month < 10:
-            month = '0'+str(month)
+            month = "{0:0>2}".format(month)
         else:
             month = str(month)
         if day < 10:
-            day = '0'+str(day)
+            day = "{0:0>2}".format(day)
         else:
             day = str(day)
+
         self.__date = str(year) + '-' + month + '-' + day
-        self.updateRanges()
+        self.__updateRanges()
         return self
 
-    ## TODO make a subtraction method
+    def __add__(self,num):
+        #intended to increment "self" by "num" days and return a new object
+        assert num >= 0
+        new = Day(self.getDate())
+        while num > 0:
+            new.addOne()
+            num -= 1
+        return new
+            
+        
 
+    ## TODO make a subtraction method
 
     def __eq__(self,other):
         return self.__date == other.getDate()
 
-    def __lt__(self,other):
+    def __gt__(self,other):
         if int(self.__date[0:4]) > int(other.getDate()[0:4]):
             return True
         elif int(self.__date[5:7]) > int(other.getDate()[5:7]):
@@ -83,7 +81,7 @@ class Day(object):
         return False
 
 
-    def __lteq__(self,other):
+    def __le__(self,other):
         return self < other or self == other
     
                 
@@ -93,7 +91,7 @@ class Day(object):
         return self.__ranges[month] == day
 
 
-    def updateRanges(self):
+    def __updateRanges(self):
         year = int(self.__date[0:4])
         #its a leap year
         if self.isLeapYear():
@@ -112,3 +110,8 @@ class Day(object):
 
     def __str__(self):
         return self.__date + ' isLeapYear: ' + str(self.isLeapYear())
+
+
+if __name__ == "__main__":
+    today = Day("2017-09-11")
+    print(today)
